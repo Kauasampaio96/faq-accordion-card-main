67a579fbecd6aba9137e7cf7e692dcfbb3874432
Kauã <kauasampaio33@hotmail.com>
2023-03-13 21:06:20 -0300

    finalização js

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const accordion = document.querySelectorAll('.accordion-single')
-const accordionContent = document.querySelector('.single-content')
+const arrow = document.querySelectorAll('.arrow')
+const title = document.querySelectorAll('.single-title')
+const accordionContent = document.querySelectorAll('.single-content')
 
-accordion.forEach(item => {
-  item.addEventListener('click', () => {
-    accordionContent.style.display = 'block'
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', () => {
+    closeAll()
+    openItem(i)
   })
-})
+}
+
+function openItem(i){
+  arrow[i].style.transform = 'rotateX(180deg)'
+  title[i].style.fontWeight = '700'
+  accordionContent[i].style.display = 'block'
+}
+
+
+function closeAll(){
+  for (let i = 0; i < accordion.length; i++){
+    arrow[i].style.transform = 'rotateX(0deg)'
+    title[i].style.fontWeight = '500'
+    accordionContent[i].style.display = 'none'
+  }
+}
